2955cbeaea7a6387776114daefaa60eaab030856
bayjidalom <bayjidalom1005@gmail.com>
2026-07-28 18:33:43 +0600

    added Python data types

---
+++ Module-01/02_data_types.py
@@ -0,0 +1,22 @@
+"""
+Python Data Types :
+1. Number
+    - integer
+    - float
+    - complex
+2. String
+3. List
+4. Tuple
+5. Dictionary
+6. Boolean - (true/false)
+"""
+
+roll = 241118
+gpa = 3.98
+name = "Bayjid Alom"
+is_passed = True
+# print(roll, gpa, name, is_passed)
+
+print(type(name))   #<class 'str'>
+print(type(roll))   #<class 'int'>
+print(type(is_passed))    #<class 'bool'>
