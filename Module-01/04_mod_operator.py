6d3ccb5a28c9f7202a2aa8c334ffe209fd560911
bayjidalom <bayjidalom1005@gmail.com>
2026-07-29 15:25:39 +0600

    added summary for module 01

---
+++ Module-01/04_mod_operator.py
@@ -0,0 +1,14 @@
+# num1 = int(input("Enter first number = "))
+# num2 = int(input("Enter second number = "))
+
+# multiply = num1 * num2;
+# print("Multiply Value is =",multiply)
+
+# summation = 10 + 20 + 15;
+# print("Summation is = ",summation)
+
+
+# Modulus (ভাগশেষ) %
+print("Reminder :", 13 % 2)
+
+
---
+++ Module-01/05_summary.py
@@ -0,0 +1,9 @@
+# VS code EXTENSIONS :
+#  Python Debugger
+# code Runner
+
+# To check version
+# 01. cmd open
+# 02. python --version
+
+print("Hello World!")
