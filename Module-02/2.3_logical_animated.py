b26f280ed683591679daaa0b9c6b1382f2f4e94e
bayjidalom <bayjidalom1005@gmail.com>
2026-07-30 23:27:09 +0600

    add short notes on Python logical operators

---
+++ Module-02/2.3_logical_animated.py
@@ -0,0 +1,22 @@
+print(True and True)    # True
+print(True and False)   # False
+print(5 > 2 and 10 > 3) # True
+print(5 > 2 and 10 < 3) # False
+
+print(True or False)    # True
+print(False or False)   # False
+print(5 > 2 or 10 < 3)  # True
+print(1 > 5 or 2 > 10)  # False
+
+print(not True)         # False
+print(not False)        # True
+print(not (5 > 2))      # False
+print(not (5 < 2))      # True
+
+
+
+"""
+➡️ উভয় শর্ত True হলে True।
+➡️ একটি শর্ত True হলেই True।
+➡️ True ↔ False করে।
+"""
