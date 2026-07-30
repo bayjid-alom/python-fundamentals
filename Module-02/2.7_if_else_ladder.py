c5249644d574188c8041dfe553bc47cce5e6a041
bayjidalom <bayjidalom1005@gmail.com>
2026-07-30 23:45:58 +0600

    add if-elif-else ladder examples

---
+++ Module-02/2.7_if_else_ladder.py
@@ -0,0 +1,10 @@
+age = 16;
+
+if age <= 12:
+    print("You are too young to enter the ghost house.")
+
+elif age >= 18:
+    print("You are allowed to enter the ghost house.")
+
+else:
+    print("You must be at least 18 years old to enter the ghost house.")
