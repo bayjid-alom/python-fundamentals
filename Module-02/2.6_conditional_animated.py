bc2509f7e5766d006adac690cc954f803ec5b8e7
bayjidalom <bayjidalom1005@gmail.com>
2026-07-30 23:39:37 +0600

    A simple program using if-else

---
+++ Module-02/2.6_conditional_animated.py
@@ -0,0 +1,10 @@
+money = int(input("Enter the amout you have : "));
+
+if money >= 100 :
+    print("We will eat burger.")
+
+elif money >= 60 :
+    print("We will eat fuchka.") 
+
+else:
+    print("We will never eat.")
