7491c3d4ddf30c4bc988a7d7df8e4f98588e6926
bayjidalom <bayjidalom1005@gmail.com>
2026-07-30 23:33:52 +0600

    If else statement

---
+++ Module-02/2.4_conditional_statement.py
@@ -0,0 +1,5 @@
+# Conditional Statement
+
+"""
+Conditional statements in Python execute different blocks of code depending on whether a specific condition evaluates to True or False. Python relies heavily on proper indentation (typically 4 spaces) rather than brackets to define the scope of these code blocks.
+"""
---
+++ Module-02/2.5_if_else.py
@@ -0,0 +1,8 @@
+money = int(input("Enter the amount of money you have : "))
+
+if money >= 200 :
+    print("I will eat burger.")
+else:
+    print("I will not eat burger")
+
+
