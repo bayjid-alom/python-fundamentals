78e6da2c6539298c1957841f3c502292103c737e
bayjidalom <bayjidalom1005@gmail.com>
2026-07-31 00:07:57 +0600

    fix: correct project path references

---
+++ Phitron/M02/2.2_conditional.py
@@ -0,0 +1,18 @@
+# this is a single line comment
+
+"""
+This is example of multiple comments.
+More comments...
+"""
+
+
+# Conditionals
+a = 10
+b = 20
+print(a > b)    
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
---
+++ Phitron/M02/2.3_if_else.py
@@ -0,0 +1,15 @@
+# Problem - 1
+
+rain = True;
+if rain:
+    print("I'll not go to school.")
+else:
+    print("I will go to school.")
+
+
+# Problem - 2
+age = 17;
+if age>=18:
+    print("You can enter the ghost house.")
+else:
+    print("You cannot enter the ghost house!!")
