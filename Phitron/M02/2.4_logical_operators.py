44c7b34a5ef5138fd1f6aa36ec9dae5b8395d4d2
bayjidalom <bayjidalom1005@gmail.com>
2026-07-31 15:43:53 +0600

    Python type Casting

--- Phitron/M02/2.4_logical_operators.py
+++ Phitron/M02/2.4_logical_operators.py
@@ -1,10 +1,27 @@
 """
 Logical Operators :
-01. and 
-02. or 
+01. and
+02. or
 03. not
 """
 
 print(10 > 20 and 5 == 5)
 print(20 < 10 or 50 >= 30)
 print(not 10 > 20)  # true
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 80:
+    print("Grade: A+")
+elif marks >= 70:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: A-")
+elif marks >= 50:
+    print("Grade: B")
+elif marks >= 40:
+    print("Grade: C")
+elif marks >= 33:
+    print("Grade: D")
+else:
+    print("Grade: F")
---
+++ Phitron/M02/2.5_typecasting.py
@@ -0,0 +1,14 @@
+# Type casting in Python
+
+age = 18
+age = float(age)
+age = int(age)
+
+if age >= 18:
+    print("You're an adult.")
+else:
+    print("You're a child.")
+
+
+print("Age Now = ", age)
+print("Type check (Age) = ", type(age))
