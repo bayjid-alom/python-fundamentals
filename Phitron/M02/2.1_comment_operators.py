695624647a343257299cbdf9e5a00ad32a7940fc
bayjidalom <bayjidalom1005@gmail.com>
2026-07-31 12:57:34 +0600

    chore: update project files

---
+++ Phitron/M02/2.1_comment_operators.py
@@ -0,0 +1,22 @@
+# This is a comment
+# This is also a commet 
+# This is example of single line comment
+
+"""
+This is example of multiple line comment.
+Something....
+"""
+
+
+
+
+# Basic Operators Example below :
+
+a = 20;
+b = 9;
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)  # Reminder
