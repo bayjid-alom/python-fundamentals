732d1281ba10156002f24b02279159a1cac630a3
bayjidalom <bayjidalom1005@gmail.com>
2026-07-31 16:52:07 +0600

    docs: add Python loop concepts

---
+++ Phitron/M04/4.1_intro_loop.py
@@ -0,0 +1,9 @@
+# Loop - Repeat anything.
+
+"""
+Types of looping :
+01. for loop
+02. while loop
+
+03. nested loop
+"""
---
+++ Phitron/M04/4.2_for_loop.py
@@ -0,0 +1,26 @@
+# For loop
+
+"""
+Concept of Python range()
+
+range() is a built-in Python function used to generate a sequence of numbers. It is commonly used with for loops.
+
+range(start, stop, step) ✅
+
+start → Starting number (default is 0)
+stop → Ending number (not included)
+step → Difference between numbers (default is 1)
+
+"""
+
+
+
+# print 0-9
+for i in range(100):
+    print("I love Python Programming.")
+
+
+for i in range(6):
+    print(i)
+
+
